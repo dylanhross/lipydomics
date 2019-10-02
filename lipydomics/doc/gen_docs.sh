b264a02753e34abcf74b6aa6489d35844bc65463
dylanhross <dhross92@uw.edu>
2019-10-02 10:32:09 -0700

    removed testing documentation1
    The test documentation is not really useful because all of the tests have the same interface, and the docstrings get printed whenever a test fails. The documentation should be a reference for the API so the test docs were just wasting space.

--- lipydomics/doc/gen_docs.sh
+++ lipydomics/doc/gen_docs.sh
@@ -4,11 +4,5 @@ python3 -m pydoc -w lipydomics
 python3 -m pydoc -w lipydomics.data
 python3 -m pydoc -w lipydomics.stats
 python3 -m pydoc -w lipydomics.plotting
-python3 -m pydoc -w lipydomics.test
-python3 -m pydoc -w lipydomics.test.__main__
-python3 -m pydoc -w lipydomics.test.data
-python3 -m pydoc -w lipydomics.test.stats
-python3 -m pydoc -w lipydomics.test.plotting
-
 
 mv -f lipydomics*.html lipydomics/doc/
