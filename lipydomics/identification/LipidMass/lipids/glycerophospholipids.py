a06259b153f3a2ff7f09b657e020013377d10be0
dylanhross <dhross92@uw.edu>
2019-10-04 12:16:40 -0700

    Added LipidMass subpackage within identification
    Added a subpackage (LipidMass) within the identification module for computing monoisotopic masses for lipids. This will be useful for generating a comprehensive theoretical database for lipids for many MS adducts.

---
+++ lipydomics/identification/LipidMass/lipids/glycerophospholipids.py
@@ -0,0 +1,268 @@
+"""
+    LipidMass/lipids/glycerophospholipids.py
+    Dylan H. Ross
+    2019/07/23
+
+    description:
+        Define individual glycerophospholipid classes
+"""
+
+
+from LipidMass.base import Glycerophospholipid
+
+
+class PA(Glycerophospholipid):
+    """
+PA
+    description:
+        phosphatidic acid lipid class
+        Glycerophospholipid with R-group =
+
+         R  =  --H
+"""
+
+    def __init__(self, sum_carbon, sum_unsaturation):
+        """
+PA.__init__
+    description:
+        Initializes an instance of a PA lipid
+    parameters:
+        sum_carbon (int) -- sum FA composition carbons
+        sum_unsaturation (int) -- sum FA composition unsaturations
+"""
+        super().__init__(sum_carbon, sum_unsaturation)
+        # the R-group for a PA is an --H
+        self.add_to_formula({'H': 1})
+        # the lipid class is PA
+        self.lipid_class = 'PA'
+
+
+class PC(Glycerophospholipid):
+    r"""
+PC
+    description:
+        phosphatidylcholine lipid class
+        Glycerophospholipid with R-group =
+
+        R  =  __    |
+                \__N(+)__
+                    |
+"""
+
+    def __init__(self, sum_carbon, sum_unsaturation):
+        """
+PC.__init__
+    description:
+        Initializes an instance of a PC lipid
+    Parameters:
+        sum_carbon (int) -- sum FA composition carbons
+        sum_unsaturation (int) -- sum FA composition unsaturations
+"""
+        super().__init__(sum_carbon, sum_unsaturation)
+        # the R-group for a PC is a choline, because of the permanent charge on the choline group, 1 H must be
+        # subtracted from the formula to account for a negative charge on the phosphate group to make the lipid
+        # have an overall neutral charge
+        self.add_to_formula({'C': 5, 'H': 12, 'N': 1})
+        # the lipid class is PC
+        self.lipid_class = 'PC'
+
+
+class PE(Glycerophospholipid):
+    r"""
+PE
+    description:
+        phosphatidylethanolamine lipid class
+        Glycerophospholipid with R-group =
+
+        R  =  __
+                \__NH2
+
+"""
+
+    def __init__(self, sum_carbon, sum_unsaturation):
+        """
+PE.__init__
+    description:
+        Initializes an instance of a PE lipid
+    Parameters:
+        sum_carbon (int) -- sum FA composition carbons
+        sum_unsaturation (int) -- sum FA composition unsaturations
+"""
+        super().__init__(sum_carbon, sum_unsaturation)
+        # the R-group for a PE is an ethanolamine (C2H6N)
+        self.add_to_formula({'C': 2, 'H': 6, 'N': 1})
+        # the lipid class is PE
+        self.lipid_class = 'PE'
+
+
+class PG(Glycerophospholipid):
+    r"""
+PG
+    description:
+        phosphatidylglycerol lipid class
+        Glycerophospholipid with R-group =
+
+        R  =  __    OH
+                \__/
+                /
+              HO
+"""
+
+    def __init__(self, sum_carbon, sum_unsaturation):
+        """
+PG.__init__
+    description:
+        Initializes an instance of a PG lipid
+    Parameters:
+        sum_carbon (int) -- sum FA composition carbons
+        sum_unsaturation (int) -- sum FA composition unsaturations
+"""
+        super().__init__(sum_carbon, sum_unsaturation)
+        # the R-group for a PG is an glycerol (C3H7O2)
+        self.add_to_formula({'C': 3, 'H': 7, 'O': 2})
+        # the lipid class is PG
+        self.lipid_class = 'PG'
+
+
+class PI(Glycerophospholipid):
+    r"""
+PI
+    description:
+        phosphatidylinositol lipid class
+        Glycerophospholipid with R-group =
+
+               HO     OH
+                 \___/
+                 /   \
+        R  =  __/     \__OH
+                \     /
+                 \___/
+                 /   \
+               HO     OH
+"""
+
+    def __init__(self, sum_carbon, sum_unsaturation):
+        """
+PI.__init__
+    description:
+        Initializes an instance of a PI lipid
+    Parameters:
+        sum_carbon (int) -- sum FA composition carbons
+        sum_unsaturation (int) -- sum FA composition unsaturations
+"""
+        super().__init__(sum_carbon, sum_unsaturation)
+        # the R-group for a PI is an inositol (C6H11O5)
+        self.add_to_formula({'C': 6, 'H': 11, 'O': 5})
+        # the lipid class is PI
+        self.lipid_class = 'PI'
+
+
+class PS(Glycerophospholipid):
+    r"""
+PS
+    description:
+        Phosphatidylserine lipid class
+        Glycerophospholipid with R-group =
+
+        R  =  __
+                \__NH2
+                /
+            O==|
+                \
+                 OH
+"""
+
+    def __init__(self, sum_carbon, sum_unsaturation):
+        """
+PS.__init__
+    description:
+        Initializes an instance of a PS lipid
+    Parameters:
+        sum_carbon (int) -- sum FA composition carbons
+        sum_unsaturation (int) -- sum FA composition unsaturations
+"""
+        super().__init__(sum_carbon, sum_unsaturation)
+        # the R-group for a PS is a serine (C3H6O2N)
+        self.add_to_formula({'C': 3, 'H': 6, 'O': 2, 'N': 1})
+        # the lipid class is PS
+        self.lipid_class = 'PS'
+
+
+class CL(Glycerophospholipid):
+    r"""
+CL
+    description:
+        Cardiolipin
+        Glycerophospholipid with core structure:
+
+                                OH
+                                |
+                  O   O__    O--P==O
+                  \\ /   \__/   |
+         O__    O--P     /      O    __O
+        /   \__/   |   HO        \__/   \
+    O==|    /      OH               \    |==O
+        \   O                        O  /
+        Ac1  \                      /  Ac4
+              |==O              O==|
+             /                      \
+           Ac2                      Ac3
+"""
+
+    def __init__(self, sum_carbon, sum_unsaturation):
+        """
+CL.__init__
+    description:
+        Sets the core formula for Cardiolipins and determines the formula contribution from the acyl chains
+    parameters:
+        sum_carbon (int) -- sum acyl carbons
+        sum_unsaturation (int) -- sum acyl unsaturations
+"""
+        # formula is initially set to the core formula (only partial head group, no fatty acid tails)
+        self.formula = {'C': 13, 'H': 18, 'O': 17, 'P': 2}
+        # two acyl chains are added to the chemical formula
+        acyl_formula = self.gen_acyl_formula(4, sum_carbon, sum_unsaturation)
+        self.add_to_formula(acyl_formula)
+        # store the sum composition
+        self.sum_composition = (sum_carbon, sum_unsaturation)
+        self.lipid_class = 'CL'
+
+
+class AcylPG(Glycerophospholipid):
+    r"""
+acylPG
+    description:
+        acylphosphatidylglycerol
+        Glycerophospholipid with core structure:
+
+
+                                   
+                   O    
+                   ||            O
+         O__    O--P--O__    O__//    
+        /   \__/   |     \__/   \
+    O==|    /      OH    /      Ac3
+        \   O          HO
+        Ac1  \
+              |==O
+             /
+           Ac2
+"""
+
+    def __init__(self, sum_carbon, sum_unsaturation):
+        """
+acylPG.__init__
+    description:
+        Sets the core formula for Cardiolipins and determines the formula contribution from the acyl chains
+    parameters:
+        sum_carbon (int) -- sum acyl carbons
+        sum_unsaturation (int) -- sum acyl unsaturations
+"""
+        # formula is initially set to the core formula (only partial head group, no fatty acid tails)
+        self.formula = {'C': 9, 'H': 12, 'O': 11, 'P': 1}
+        # two acyl chains are added to the chemical formula
+        acyl_formula = self.gen_acyl_formula(3, sum_carbon, sum_unsaturation)
+        self.add_to_formula(acyl_formula)
+        # store the sum composition
+        self.sum_composition = (sum_carbon, sum_unsaturation)
+        self.lipid_class = 'acylPG'
---
+++ lipydomics/identification/LipidMass/lipids/lysoglycerophospholipids.py
@@ -0,0 +1,189 @@
+"""
+    LipidMass/lipids/lysoglycerophospholipids.py
+    Dylan H. Ross
+    2019/07/23
+
+    description:
+        Define individual lysoglycerophospholipid classes
+"""
+
+
+from LipidMass.base import Lysoglycerophospholipid
+
+
+class LPA(Lysoglycerophospholipid):
+    """
+LPA
+    description:
+        Lysophosphatidic acid lipid class
+        Lysoglycerophospholipid with R-group =
+
+         R  =  --H
+"""
+
+    def __init__(self, sum_carbon, sum_unsaturation):
+        """
+LPA.__init__
+    description:
+        Initializes an instance of a LPA lipid
+    parameters:
+        sum_carbon (int) -- sum FA composition carbons
+        sum_unsaturation (int) -- sum FA composition unsaturations
+"""
+        super().__init__(sum_carbon, sum_unsaturation)
+        # the R-group for a LPA is an --H
+        self.add_to_formula({'H': 1})
+        # the lipid class is LPA
+        self.lipid_class = 'LPA'
+
+
+class LPC(Lysoglycerophospholipid):
+    r"""
+LPC
+    description:
+        Lysophosphatidylcholine lipid class
+        Lysoglycerophospholipid with R-group =
+
+        R  =  __    |
+                \__N(+)__
+                    |
+"""
+
+    def __init__(self, sum_carbon, sum_unsaturation):
+        """
+LPC.__init__
+    description:
+        Initializes an instance of a LPC lipid
+    Parameters:
+        sum_carbon (int) -- sum FA composition carbons
+        sum_unsaturation (int) -- sum FA composition unsaturations
+"""
+        super().__init__(sum_carbon, sum_unsaturation)
+        # the R-group for a LPC is a choline, because of the permanent charge on the choline group, 1 H must be
+        # subtracted from the formula to account for a negative charge on the phosphate group to make the lipid
+        # have an overall neutral charge
+        self.add_to_formula({'C': 5, 'H': 12, 'N': 1})
+        # the lipid class is LPC
+        self.lipid_class = 'LPC'
+
+
+class LPE(Lysoglycerophospholipid):
+    r"""
+LPE
+    description:
+        Lysophosphatidylethanolamine lipid class
+        Lysoglycerophospholipid with R-group =
+
+        R  =  __
+                \__NH2
+
+"""
+
+    def __init__(self, sum_carbon, sum_unsaturation):
+        """
+LPE.__init__
+    description:
+        Initializes an instance of a LPE lipid
+    Parameters:
+        sum_carbon (int) -- sum FA composition carbons
+        sum_unsaturation (int) -- sum FA composition unsaturations
+"""
+        super().__init__(sum_carbon, sum_unsaturation)
+        # the R-group for a LPE is an ethanolamine (C2H6N)
+        self.add_to_formula({'C': 2, 'H': 6, 'N': 1})
+        # the lipid class is LPE
+        self.lipid_class = 'LPE'
+
+
+class LPG(Lysoglycerophospholipid):
+    r"""
+LPG
+    description:
+        Lysophosphatidylglycerol lipid class
+        Lysoglycerophospholipid with R-group =
+
+        R  =  __    OH
+                \__/
+                /
+              HO
+"""
+
+    def __init__(self, sum_carbon, sum_unsaturation):
+        """
+LPG.__init__
+    description:
+        Initializes an instance of a LPG lipid
+    Parameters:
+        sum_carbon (int) -- sum FA composition carbons
+        sum_unsaturation (int) -- sum FA composition unsaturations
+"""
+        super().__init__(sum_carbon, sum_unsaturation)
+        # the R-group for a LPG is an glycerol (C3H7O2)
+        self.add_to_formula({'C': 3, 'H': 7, 'O': 2})
+        # the lipid class is LPG
+        self.lipid_class = 'LPG'
+
+
+class LPI(Lysoglycerophospholipid):
+    r"""
+LPI
+    description:
+        Lysophosphatidylinositol lipid class
+        Lysoglycerophospholipid with R-group =
+
+               HO     OH
+                 \___/
+                 /   \
+        R  =  __/     \__OH
+                \     /
+                 \___/
+                 /   \
+               HO     OH
+"""
+
+    def __init__(self, sum_carbon, sum_unsaturation):
+        """
+LPI.__init__
+    description:
+        Initializes an instance of a LPI lipid
+    Parameters:
+        sum_carbon (int) -- sum FA composition carbons
+        sum_unsaturation (int) -- sum FA composition unsaturations
+"""
+        super().__init__(sum_carbon, sum_unsaturation)
+        # the R-group for a LPI is an inositol (C6H11O5)
+        self.add_to_formula({'C': 6, 'H': 11, 'O': 5})
+        # the lipid class is LPI
+        self.lipid_class = 'LPI'
+
+
+class LPS(Lysoglycerophospholipid):
+    r"""
+LPS
+    description:
+        Lysophosphatidylserine lipid class
+        Lysoglycerophospholipid with R-group =
+
+        R  =  __
+                \__NH2
+                /
+            O==|
+                \
+                 OH
+"""
+
+    def __init__(self, sum_carbon, sum_unsaturation):
+        """
+LPS.__init__
+    description:
+        Initializes an instance of a LPS lipid
+    Parameters:
+        sum_carbon (int) -- sum FA composition carbons
+        sum_unsaturation (int) -- sum FA composition unsaturations
+"""
+        super().__init__(sum_carbon, sum_unsaturation)
+        # the R-group for a LPS is a serine (C3H6O2N)
+        self.add_to_formula({'C': 3, 'H': 6, 'O': 2, 'N': 1})
+        # the lipid class is LPS
+        self.lipid_class = 'LPS'
+
