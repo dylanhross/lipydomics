c029ab4839158f98e850ed8529d30e6d389d9206
dylanhross <dhross92@uw.edu>
2019-11-26 11:35:06 -0800

    had to edit the lipid parser to recognize PIP2 and PIP3
    building the lipids.db database was failing because the lipid parser was failing to parse PIP2 and PIP3 lipid names. This was happening because the regex for the lipid class would not recognize any numbers... added 2 and 3 to that part of the regex. Rebuilt the database and everything looks good now.

--- lipydomics/identification/lipid_parser.py
+++ lipydomics/identification/lipid_parser.py
@@ -41,7 +41,7 @@ parse_lipid
     parsed = {}
 
     # compile regex pattern
-    l_pat = re.compile(r"^(?P<cls>[A-Za-z]+)\((?P<mod>[pdoe]*)(?P<fc1>[0-9]+):(?P<fu1>[0-9]+)/*((?P<fc2>[0-9]+):(?P<fu2>[0-9]+))*/*((?P<fc3>[0-9]+):(?P<fu3>[0-9]+))*\)")
+    l_pat = re.compile(r"^(?P<cls>[A-Za-z23]+)\((?P<mod>[pdoe]*)(?P<fc1>[0-9]+):(?P<fu1>[0-9]+)/*((?P<fc2>[0-9]+):(?P<fu2>[0-9]+))*/*((?P<fc3>[0-9]+):(?P<fu3>[0-9]+))*\)")
 
     # parse the name using regex
     l_res = l_pat.match(name)
