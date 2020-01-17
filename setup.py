1ed23c0fd7f528955fd4af93d03e87ed3a3b7b20
dylanhross <dhross92@uw.edu>
2020-01-17 12:28:34 -0800

    Update setup.py
    as of now, VERSION 1.0.0
    Major version (API changes): 1
    Minor version (back-compatible feature changes): 0
    Micro version (back-compatible bug fixes): 0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='lipydomics',
-    version='1.0.0a1',
+    version='1.0.0',
     author='Dylan H. Ross',
     author_email='dhross92@uw.edu',
     description='a library for streamlining lipidomics data analysis',
