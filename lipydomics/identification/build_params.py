05e2f3f6f5a6fd1b3a961e5a0e450bff196186b8
dylanhross <dhross92@uw.edu>
2021-08-10 14:30:49 -0700

    removed Vasilopoulou et al. from database
    removed Vasilopoulou et al. from database based on issues raised in this paper:
    https://www.nature.com/articles/s41467-021-24984-y

--- lipydomics/identification/build_params.py
+++ lipydomics/identification/build_params.py
@@ -7,6 +7,23 @@
         defines parameters to use for building the database:
             include_ref_dsets - reference datasets to include in the database
             ccs_pred_ref_dsets - reference datasets to use for CCS prediction
+
+
+    NOTE:
+        Vasilopoulou et al. data excluded from database build and CCS predictive model
+        based on issues raised in:
+
+        Nat. Comm. (2021) 12:4771
+        Quality control requirements for the correctannotation of lipidomics data
+        Harald C. Köfeler, Thomas O. Eichmann, Robert Ahrends, John A. Bowden,
+        Niklas Danne-Rasche, Edward A. Dennis, Maria Fedorova, William J. Griffiths,
+        Xianlin Han, Jürgen Hartler, Michal Holčapek, Robert Jirásko, Jeremy P.
+        Koelmel, Christer S. Ejsing, Gerhard Liebisch, Zhixu Ni,
+        Valerie B. O’Donnell, Oswald Quehenberger, Dominik Schwudke
+        Andrej Shevchenko, Michael J. O. Wakelam, Markus R. Wenk, Denise Wolrab
+        & Kim Ekroos
+
+        ARISING  FROM M. Mann et al. Nature  Communications https://doi.org/10.1038/s41467-019-14044-x(2020).
 """
 
 
@@ -18,9 +35,10 @@ include_ref_dsets = [
     "hine0119",
     'leap0219',
     'blaz0818',
-    'vasi0120_pos',
-    'vasi0120_neg',
-    'vasi0120_neg_corr',
+    # See note in description...
+    #'vasi0120_pos',
+    #'vasi0120_neg',
+    #'vasi0120_neg_corr',
     'tsug0220_pos',
     'tsug0220_neg',
     'tsug0220_neg_corr',
@@ -36,8 +54,9 @@ ccs_pred_ref_dsets = [
     "hine0119",
     'leap0219',
     'blaz0818',
-    'vasi0120_pos',
-    'vasi0120_neg_corr'
+    # See note in description...
+    #'vasi0120_pos',
+    #'vasi0120_neg_corr'
     'tsug0220_pos',
     'tsug0220_neg_corr',
     'hine0520'
